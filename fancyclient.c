70152c1c767e47bd73ac12b557af0804f0920cde
Omar Ben Said <omar.ben-said@fiat.polytechnique.fr>
2022-03-18 14:51:33 +0100

    fixed it

--- fancyclient.c
+++ fancyclient.c
@@ -25,17 +25,7 @@ int main(int argc, char* argv[])
 	inet_pton(AF_INET,IP,&to.sin_addr.s_addr);
 	to.sin_port  = htons(atoi(port));
 	int sockto = socket(AF_INET, SOCK_DGRAM, 0);
-	struct sockaddr_in from;
-	memset(&from, 0, sizeof(from));
-	from.sin_family = AF_INET;
-	inet_pton(AF_INET,IP,&from.sin_addr.s_addr);
-	from.sin_port = htons(atoi(port));
-	int sockfrom = socket(AF_INET, SOCK_DGRAM, 0);
-    if (bind(sockfrom,(struct sockaddr*)&from, sizeof(from))){
-		fprintf(stderr,"failed to bind\n");
-		return 1;
-	}
-	int sizeofsocket=sizeof(from);
+	int sizeofsocket=sizeof(to);
 	char data_received[256];
 	char data_to_send[256];
 	while(1){
@@ -45,8 +35,8 @@ int main(int argc, char* argv[])
 	int bytes_sent = sendto(sockto, data_to_send, strlen(data_to_send), 0,
            (struct sockaddr*)&to, sizeof(to));
 	memset(data_received,0,256);
-	int bytes_received = recvfrom(sockfrom, data_received, 255, 0,
-           (struct sockaddr*)&from, &sizeofsocket);
+	int bytes_received = recvfrom(sockto, data_received, 255, 0,
+           (struct sockaddr*)&to, &sizeofsocket);
 	printf("data received is: %s\n",data_received);
 	}
 	return (0);
--- server.c
+++ server.c
@@ -24,16 +24,18 @@ int main(int argc, char* argv[])
 	inet_pton(AF_INET,"127.0.0.1",&from.sin_addr.s_addr);
 	from.sin_port = htons(atoi(port));
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (bind(sockfd,(struct sockaddr*)&from, sizeof(from))){
+	int sizeofsocket=sizeof(from);
+    if (bind(sockfd,(struct sockaddr*)&from, sizeofsocket)){
 		printf("failed to bind\n");
 		return 1;
 	}
-	int sizeofsocket=sizeof(from);
 	char data_received[256];
 	while(1){
 	memset(data_received,0,256);
 	int bytes_received = recvfrom(sockfd, data_received, 255, 0,
            (struct sockaddr*)&from, &sizeofsocket);
+	int bytes_sent = sendto(sockfd, data_received, strlen(data_received), 0,
+           (struct sockaddr*)&from, sizeofsocket);
 	printf("%s\n",data_received);
 	}
 	return (0);
